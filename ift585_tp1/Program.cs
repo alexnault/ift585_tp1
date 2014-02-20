74ef078c37c2fcb51143fa5e3f6327f2a81734f4
RoyAlex <alexandre.roy90@gmail.com>
2014-02-20 18:21:54 -0500

    Fichier de commande en release

--- ift585_tp1/Program.cs
+++ ift585_tp1/Program.cs
@@ -11,31 +11,62 @@ namespace ift585_tp1
     {
         static void Main(string[] args)
         {
+            bool isParam = true;
             // TODO (Cisco) : Make app params work as intended
-
-            /////PROGRAM PARAMS////
-            // La taille	du tampon utilisé de chaque	côté du	thread simulant le réseau (tampon d'envoi).
-            int bufferLength = 3;
-            // Le délai de temporisation	(time-out) de l'émetteur.
-		    int timeout = 1000;
-            // Le fichier à copier
-            string file = "./input.txt";
-            // L'emplacement de destination	pour la	copie du fichier.
-            string savePath = "./blabla/out/chat.txt";
-            ///////////////////////
-
-            Network network = new Network();
-            EndDevice receiver = new EndDevice(network, bufferLength, null, savePath, timeout);
-            EndDevice emitter = new EndDevice(network, bufferLength, file, null, timeout);
+            #if (DEBUG)
+                /////PROGRAM PARAMS////
+                // La taille	du tampon utilisé de chaque	côté du	thread simulant le réseau (tampon d'envoi).
+                int bufferLength = 3;
+                // Le délai de temporisation	(time-out) de l'émetteur.
+		        int timeout = 1000;
+                // Le fichier à copier
+                string file = "./input.txt";
+                // L'emplacement de destination	pour la	copie du fichier.
+                string savePath = "./blabla/out/chat.txt";
+                ///////////////////////
+            #else
             
+            int bufferLength = 0;
+            int timeout = 0;
+            int windowSize = 0;
+            string protocolType = "";
+            string errorType = "";
+            string file = "";
+            string savePath = "";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("args is null"); // Check for null array
+                isParam = false;
+            }
+            else
+            {
+                bufferLength = Convert.ToInt32(args[0]);
+                timeout = Convert.ToInt32(args[1]);
+                file = args[2];
+                savePath = args[3];
+                //windowSize = Convert.ToInt32(args[4]);
+                //protocolType = args[5];
+                //errorType = args[6];
+            }
+            #endif
 
-            Thread networkThread = new Thread(new ThreadStart(network.Start));
-            Thread receiverThread = new Thread(new ThreadStart(receiver.Start));
-            Thread emitterThread = new Thread(new ThreadStart(emitter.Start));
+            if (isParam)
+            {
+                Network network = new Network();
+                EndDevice receiver = new EndDevice(network, bufferLength, null, savePath, timeout);
+                EndDevice emitter = new EndDevice(network, bufferLength, file, null, timeout);
 
-            networkThread.Start();
-            receiverThread.Start();
-            emitterThread.Start();
+
+                Thread networkThread = new Thread(new ThreadStart(network.Start));
+                Thread receiverThread = new Thread(new ThreadStart(receiver.Start));
+                Thread emitterThread = new Thread(new ThreadStart(emitter.Start));
+
+                networkThread.Start();
+                receiverThread.Start();
+                emitterThread.Start();
+            }
+            else
+                Console.ReadLine();
         }
     }
 }
